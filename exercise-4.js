579c6f97d0e550d7a13c39bdd548b493e67d9dc3
huichingheng <huichingheng@gmail.com>
2018-09-02 21:13:10 +0800

    1st commit

--- exercise-4.js
+++ exercise-4.js
@@ -1,43 +1,70 @@
 // 1. Create an array `names` with at least 3 elements: `'david'`, `'gordon'`, and
 //    `'sahil'` and log it to the console
-
+const names = [
+    'david',
+    'gordon',
+    'sahil'
+]
+console.log('1. ', names);
 
 // 2. Log the value of the third element in `names` to the console
-
+console.log('2. ', names[2]);
 
 // 3. Log the value of the `names` array's `length` property to the console
-
+console.log('3. ', names.length);
 
 // 4. Set the value of the `length` property to `1`
-
+names.length = 1;
 
 // 6. Once again, log the value of the `names` array's `length` property to the
 //    console. Did it change?
-
+console.log('6. ', names.length);
 
 // 7. Log the entire `names` array to the console again
-
+console.log('7. ', names);
 
 // 8. Add a few more elements to the array using `push`
-
+names.push('yulian', 'minzhuang', 'mayuri', 123, true);
+console.log('8. ', names.length)
+console.log('8. ', names)
 
 // 9. Log the last element of the array to the console
-
+// console.log(names[]);
+console.log(names[5]);
 
 // 10. Log the second last element of the array to the console
-
+console.log(names[names.length-2])
 
 // 11. Change the `textContent` property of the _second_ `p` tag on the page to
 //    `"JavaScript was here!"`
-
+document.querySelectorAll('p')[1].textContent = 'JavaScript was here!'
+console.log(document.querySelectorAll('p')[1])
 
 // 12. Convert the `names` array to a string, with each element separated by a
 //    '<br>'. Store it in a new variable called `namesString` e.g. if `names` is
 //    `['harry', 'ron', 'dean']` then `namesString` will be `"harry<br>ron<br>dean"`
-
+const nameString = names.join("<br>")
+console.log('12. ', nameString)
 
 // 13. Change the `innerHTML` property of the _first_ `p` tag on the page to
 //    `namesString`. What happens when you use `textContent` instead of
 //    `innerHTML`?
+// document.querySelectorAll('p')[0].innerHTML = 'namesString'
+// console.log(document.querySelectorAll('p')[0])
+// document.querySelectorAll('p')[0].textContent = 'namesString'
+// console.log(document.querySelectorAll('p')[0])
+const firstTag = document.querySelectorAll('p')[0]
+firstTag.innerHTML = 'namesString'
+console.log(firstTag.innerHTML)
+
+firstTag.textContent = 'namesString'
+console.log(firstTag.textContent)
 
 
+let function1 = function() {
+    console.log('From function 1');
+}
+
+function function2() {
+    console.log('From function 2');
+}
--- exercise-5.js
+++ exercise-5.js
@@ -1,17 +1,45 @@
-// 1. Create a new array `shoppingList` with a few string elements
+console.log('Exercise-5:')
 
+// 1. Create a new array `shoppingList` with a few string elements
+const shoppingList = ['carrot','tomato','potato']
 
 // 2. Using `forEach`, log every element of the `shoppingList` array to the console
-
+shoppingList.forEach(function (item) {
+    console.log(item);
+})
+// OR shoppingList.forEach(item => console.log(item));
 
 // 3. For each element in the `shoppingList` array, create an `<li>` node and
 //    append it to the `<ul>` element on the page (use `document.createElement`
 //    and `.appendChild`)
+const ul = document.querySelector('ul') 
 
+shoppingList.forEach(function (item) {
+
+    const li = document.createElement('li')
+    li.textContent = item
+    
+    ul.appendChild(li)
+        
+
+}) 
+
+console.log(ul)
 
 // 4. Create a new array `numbers` with a few numeric elements
-
+const numbers = [1, 2, 4.5, -10.25]
 
 // 5. Using `map` on the `numbers` array, create a new array called
 //    `squareNumbers` where each element is the square of the corresponding
 //    element in the `numbers` array
+// const squareNumbers = numbers.map(function (number) {
+//     return number*number;
+// })
+const squareNumbers = numbers.map(x => x*x);
+console.log(squareNumbers)
+
+// const squareNumbers = numbers.map(number => number ** 2);
+
+// console.log('numbers:', numbers);
+// console.log('squareNumbers:', squareNumbers);
+
